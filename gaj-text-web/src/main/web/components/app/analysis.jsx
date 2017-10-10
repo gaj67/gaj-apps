dc0a4b82e04ccf18a9f658d34b01a1b66643bc86
Geoff Jarrad <geoff.jarrad@d2dcrc.com.au>
2017-10-10 15:25:53 +1030

    Added simple IO to analysis.

--- gaj-text-web/src/main/web/components/app/analysis.jsx
+++ gaj-text-web/src/main/web/components/app/analysis.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import axios from 'axios'
 
 class Analysis extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: ''
+            input: '',
+            output: ''
         };
         
         this.handleChange = this.handleChange.bind(this);
@@ -12,20 +14,39 @@ class Analysis extends React.Component {
     }
     
     handleChange(event) {
-        this.setState({value: event.target.value});
+        this.setState({input: event.target.value});
     }
     
     handleSubmit(event) {
-        alert('Text submitted: ' + this.state.value);
+        alert('Text submitted: ' + this.state.input);
         event.preventDefault();
+        this.setState({output: this.state.input});
+        /*
+        axios.get(
+            'api/tokenise?text=' + encodeURIComponent(this.state.input)
+        ).then(output => {
+            alert('Response received: ' + output);
+            this.setState({output: output});
+        }).catch(error => {
+            console.log(error);
+        }); 
+        */
     }
     
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <label>Text input:</label>
-                <br/>
-                <textarea cols={100} rows={5} value={this.state.value} onChange={this.handleChange} />
+                <div>
+                    <label>Text input:</label>
+                    <br/>
+                    <textarea cols={100} rows={5} value={this.state.input} onChange={this.handleChange} />
+                </div>
+                <div className={(this.state.output == '') ? 'show-none' : 'show-block'}>
+                    <br/>
+                    <label>Analysed output:</label>
+                    <br/>
+                    <textarea cols={100} rows={5} value={this.state.output} readOnly />
+                </div>
                 <br/>
                 <input type="submit" value="Analyse" />
             </form>
