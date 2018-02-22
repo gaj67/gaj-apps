06e92c647b3029464543489838f7b9df81aac0ae
Geoff Jarrad <geoff.jarrad@d2dcrc.com.au>
2018-02-22 16:10:46 +1030

    Trying to create maven project from scratch.

---
+++ nc.analyser/src/test/java/apps/gaj/nc/analyser/AppTest.java
@@ -0,0 +1,19 @@
+package apps.gaj.nc.analyser;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+
+}
